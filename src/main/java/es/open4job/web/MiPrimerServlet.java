76a52abda82565e531c5579bda1f6b1e1b17c15c
lchacon <lchaconperez@gmail.com>
2015-02-03 10:12:02 +0100

    Sacar detalle por web en servlet

--- src/main/java/es/open4job/web/MiPrimerServlet.java
+++ src/main/java/es/open4job/web/MiPrimerServlet.java
@@ -8,39 +8,70 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.open4job.modelo.dao.AparcamientoMotoDAO;
+import es.open4job.modelo.vo.AparcamientoMotoVO;
+
 /**
  * Servlet implementation class MiPrimerServlet
  */
 public class MiPrimerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public MiPrimerServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter out = response.getWriter();
-		String respuesta=request.getParameter("id");
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h1>C'est fini Mundo</h1>");
-		out.println("<h1>"+ respuesta +"</h1>");
-		out.println("</body>");
-		out.println("</html>");
-		
+	public MiPrimerServlet() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		int id=0;
+		try{
+			id = Integer.parseInt(request.getParameter("id"));
+		}catch(Exception e){
+			
+		}
+
+		String driver = "oracle.jdbc.driver.OracleDriver";
+
+		String host = "54.154.192.80";
+		String puerto = "1521";
+		String sid = "xe";
+
+		String user = "test";
+		String password = "test";
+
+		String url = "jdbc:oracle:thin:" + user + "/" + password + "@" + host
+				+ ":" + puerto + ":" + sid;
+
+		// AparcamientoMoto
+		AparcamientoMotoDAO aparcamientoMotoDAO = new AparcamientoMotoDAO(
+				driver, url, user, password);
+		// Detalles Aparcamiento Moto
+		AparcamientoMotoVO motoDetalle = aparcamientoMotoDAO
+				.getDetailAparcamientoMoto(id);
+
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>AparcamientoMoto " + motoDetalle.toString() + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
